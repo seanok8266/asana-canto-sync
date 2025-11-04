62ef17e1eb1756633b9d8b87c332fdba8ee14dfc
Sean OKelley <sean@thedamconsultants.com>
2025-11-04 15:21:40 -0500

    Add Asana connect route

--- src/server.js
+++ src/server.js
@@ -7,7 +7,18 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Asana ↔ Canto Sync Service Running");
+    res.send("Asana ↔ Canto Sync Service Running");
+});
+
+app.get("/connect/asana", (req, res) => {
+    const clientId = process.env.ASANA_CLIENT_ID;
+    const redirectUri = process.env.ASANA_REDIRECT_URI;
+
+    const authUrl = `https://app.asana.com/-/oauth_authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
+        redirectUri
+    )}&response_type=code`;
+
+    res.redirect(authUrl);
 });
 
 const port = process.env.PORT || 3000;
