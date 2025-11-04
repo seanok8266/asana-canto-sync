6fc44bda637125783d4f53a36a96008777c3f5e9
Sean OKelley <sean@thedamconsultants.com>
2025-11-04 15:49:39 -0500

    Add Asana OAuth callback route

--- src/server.js
+++ src/server.js
@@ -7,18 +7,24 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Asana ↔ Canto Sync Service Running");
+  res.send("Asana ↔ Canto Sync Service Running");
 });
 
 app.get("/connect/asana", (req, res) => {
-    const clientId = process.env.ASANA_CLIENT_ID;
-    const redirectUri = process.env.ASANA_REDIRECT_URI;
+  const clientId = process.env.ASANA_CLIENT_ID;
+  const redirectUri = process.env.ASANA_REDIRECT_URI;
 
-    const authUrl = `https://app.asana.com/-/oauth_authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
-        redirectUri
-    )}&response_type=code`;
+  const authUrl = `https://app.asana.com/-/oauth_authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
+    redirectUri
+  )}&response_type=code`;
 
-    res.redirect(authUrl);
+  res.redirect(authUrl);
+});
+
+app.get("/oauth/callback/asana", (req, res) => {
+  const authCode = req.query.code;
+  if (!authCode) return res.status(400).send("Missing authorization code.");
+  res.send("Asana authorization successful! (We will exchange the code next.)");
 });
 
 const port = process.env.PORT || 3000;
