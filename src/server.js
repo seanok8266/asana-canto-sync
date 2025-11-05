a06591517f6d098000f58fe25362c68879c9c906
Sean OKelley <sean@thedamconsultants.com>
2025-11-05 17:43:45 -0500

    update tenant domain

--- src/server.js
+++ src/server.js
@@ -405,8 +405,7 @@ app.post("/test/upload-canto", async (req, res) => {
       return res.status(400).send("Canto token not found for this domain.");
     }
 
-  const uploadUrl = `https://oauth.canto.com/api/v1/import/upload`;
-
+  const uploadUrl = `https://${domain}/api/v1/import/upload`;
     console.log("📤 Uploading to:", uploadUrl);
 
     try {
